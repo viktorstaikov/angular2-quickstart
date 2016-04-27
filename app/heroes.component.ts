1fdeb03081b7965e6298984eede03847af55a0db
Viktor Staykov <viktor.staikov@gmail.com>
2016-04-27 14:25:21 +0300

    done

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from 'angular2/core';
+import {Router} from 'angular2/router';
+
 import {HeroDetailComponent} from './hero-detail.component';
 import {Hero} from './hero';
 import {HeroService} from './hero.service';
@@ -10,17 +12,26 @@ import {HeroService} from './hero.service';
     directives: [HeroDetailComponent]
 })
 export class HeroesComponent implements OnInit {
-	title = "Tour of Heroes";
 	selectedHero: Hero;
 	heroes: Hero[];
-	constructor(private _heroService: HeroService) { }
-	ngOnInit() {
-		this.getHeroes();
-	}
+
+	constructor(
+		private _heroService: HeroService,
+		private _router: Router
+	) { }
 	getHeroes() {
 		this._heroService.getHeroes().then(heroes => this.heroes = heroes);
 	}
+
+	ngOnInit() {
+		this.getHeroes();
+	}
+	
 	onSelect(hero: Hero) {
 		this.selectedHero = hero;
+	}
+	
+	gotoDetail() {
+		this._router.navigate(['HeroDetail', { id: this.selectedHero.id }]);
 	}
 }
